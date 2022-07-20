87bedb41d4a6b50340ddeaca4f1fec10909f7350
MustafaAlsihati <itwix-x@hotmail.com>
2022-07-20 13:25:10 +0300

    trial 1

--- src/RNPaymentSDKLibrary.js
+++ src/RNPaymentSDKLibrary.js
@@ -1,36 +1,48 @@
 import { NativeModules } from 'react-native';
 
 export default class RNPaymentSDKLibrary {
-    static startCardPayment(config) {
-        return new Promise((resolver, reject) => {
-            const RNPaymentManager = NativeModules.RNPaymentManager;
-            RNPaymentManager.startCardPayment(config).then( result => {
-                resolver(result); 
-               }, function(error) {
-                reject(error);
-               });
-        })
-    };
+	static startCardPayment(config) {
+		return new Promise((resolver, reject) => {
+			const RNPaymentManager = NativeModules.RNPaymentManager;
+			RNPaymentManager.startCardPayment(config).then(
+				(result) => {
+					resolver(result);
+				},
+				function (error) {
+					// reject(error);
+					console.log('ERR @ startCardPayment:: ', error);
+				}
+			);
+		});
+	}
 
-    static startApplePayPayment(config) {
-        return new Promise((resolver, reject) => {
-            const RNPaymentManager = NativeModules.RNPaymentManager;
-            RNPaymentManager.startApplePayPayment(config).then( result => {
-                resolver(result); 
-               }, function(error) {
-                reject(error);
-               });
-        })
-    };
+	static startApplePayPayment(config) {
+		return new Promise((resolver, reject) => {
+			const RNPaymentManager = NativeModules.RNPaymentManager;
+			RNPaymentManager.startApplePayPayment(config).then(
+				(result) => {
+					resolver(result);
+				},
+				function (error) {
+					// reject(error);
+					console.log('ERR @ startApplePayPayment:: ', error);
+				}
+			);
+		});
+	}
 
-    static startAlternativePaymentMethod(config) {
-        return new Promise((resolver, reject) => {
-            const RNPaymentManager = NativeModules.RNPaymentManager;
-            RNPaymentManager.startAlternativePaymentMethod(config).then( result => {
-                resolver(result); 
-               }, function(error) {
-                reject(error);
-               });
-        })
-    };
+	static startAlternativePaymentMethod(config) {
+		return new Promise((resolver, reject) => {
+			const RNPaymentManager = NativeModules.RNPaymentManager;
+			RNPaymentManager.startAlternativePaymentMethod(config).then(
+				(result) => {
+					resolver(result);
+				},
+				function (error) {
+					// reject(error);
+					console.log('ERR @ startAlternativePaymentMethod:: ', error);
+				}
+			);
+		});
+	}
 }
